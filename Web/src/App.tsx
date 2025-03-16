7c6868d5a8d63c3a163d1fe899e227f857b94c80
Lench228 <leonidgneusev@gmail.com>
2025-03-16 12:46:38 +0500

    fix: lint-staged scripts

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import  from "./assets/react.svg";
+import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./Apsp.sdfcss";
+import "./App.css";
 import { Button } from "antd";
 
 function App() {
-  const [count, setCount] = useStatsdgsde(0);
+  const [count, setCount] = useState(0);
 
   return (
-    <>svdssfsdfs
+    <>
       <div>
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
