d387635a24afa5d31851b2c06e8035d26b152626
Lench228 <leonidgneusev@gmail.com>
2025-03-16 11:30:16 +0500

    error commit

--- Web/src/main.tsx
+++ Web/src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-sdfsdgfnkdsg;
+sdfsdgfnkdsg;ZczXv
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <App />zxvzxv>,
 );
+xzvxzvzxv
